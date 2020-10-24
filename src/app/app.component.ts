9d9507bb56e0b70f11bd32d62bf2d45b5c6bff73
ohalliday <owenhalliday1@gmail.com>
2020-10-24 10:17:44 +0100

    toolbar logo and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'hero-page';
+  title = "hero-page";
+
+  links = [
+    "Ethos & Values",
+    "Who we help",
+    "How do we do it",
+    "Why it works",
+    "Get in touch",
+  ];
 }
